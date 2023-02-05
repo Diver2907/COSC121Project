ae620d931cab96575de9c591eb20c1b3514b818a
Cooper Ross <c.mross2907@gmail.com>
2023-02-05 10:44:10 -0800

    Moved items

---
+++ P2/FarmTest (1).java
@@ -0,0 +1,25 @@
+package P2;
+
+import java.util.Arrays;
+
+public class FarmTest {
+	public static void main(String[] args) {
+		Farm myFarm = new Farm();
+		myFarm.add(new Chicken());
+		myFarm.add(new Cow());
+		for(Animal a:myFarm.getAnimals()) {
+			if (a == null) {break;}
+			else {
+				for(int i=0; i<(int)(Math.random()*1000); i++) {
+					a.move();
+				}
+			}
+		}
+		System.out.println("Before feeding: " + myFarm.getAvailableFood());
+		myFarm.feedAnimals();
+		System.out.println("After feeding: " + myFarm.getAvailableFood());
+		myFarm.makeNoise();
+		myFarm.getAnimals()[0].speak("Hello!!");
+		System.out.println(Arrays.toString(myFarm.getAnimals()));
+	}
+}
