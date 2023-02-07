222b1d7a59ef02bdd85981e0449b466d7f5675bd
Cooper Ross <c.mross2907@gmail.com>
2023-02-06 17:40:14 -0800

    Added new code

--- P2/FarmTest.java
+++ P2/FarmTest.java
@@ -2,31 +2,21 @@ package P2;
 
 import java.util.Arrays;
 
-public class FarmTest implements Cloneable{
+public class FarmTest {
 	public static void main(String[] args) throws CloneNotSupportedException {
 		Farm myFarm = new Farm();
-		myFarm.add(new Chicken());
-		myFarm.add(new Cow());
-		myFarm.add(new Llama());
-		myFarm.add(new Llama());
-		for(Animal a:myFarm.getAnimals()) {
-			if (a == null) {break;}
-			else {
-				for(int i=0; i<(int)(Math.random()*1000); i++) {
-					a.move();
-				}
-			}
+		for(Animal a: myFarm.getAnimals()){
+			a.setEnergy(Math.random()*100);
 		}
+		System.out.println("\nInitial list of animals:\n-------------------------");
+		myFarm.printAnimals();
+		System.out.println("\nAdding a clone of the second animal\n-----------------------------------");
+		myFarm.addClone(myFarm.getAnimals()[1]);
+		myFarm.printAnimals();
+		System.out.println("\nAfter SORTING:\n--------------");
 		myFarm.animSort();
-		System.out.println(Arrays.toString(myFarm.getAnimals())); 
-		System.out.println("Before feeding: " + myFarm.getAvailableFood());
-		myFarm.feedAnimals();
-		System.out.println("After feeding: " + myFarm.getAvailableFood());
-		myFarm.makeNoise();
-		myFarm.getAnimals()[0].speak("Hello!!");
-		System.out.println(Arrays.toString(myFarm.getAnimals()));
-		System.out.println("Before Clone:");
-		System.out.println(myFarm.addClone(myFarm.getAnimals()[2]));
-		System.out.println("After clone "+Arrays.toString(myFarm.getAnimals()));
+		myFarm.printAnimals();
+		System.out.println("\nFarm summary:\n--------------");
+		myFarm.printSummary();
 	}
 }
